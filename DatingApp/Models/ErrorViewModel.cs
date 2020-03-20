c264186c1d7aa9d69e5c117d866dbf4a70f5ec75
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 11:28:35 +0100

    added mvc template

---
+++ DatingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DatingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
