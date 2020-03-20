3a91a3521c0ff02fdbba6c86897c52e4fc26ddbe
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 14:25:12 +0100

    added years counter test

---
+++ DatingApp.Tests/YearsCountTests.cs
@@ -0,0 +1,23 @@
+﻿using DatingApp.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DatingApp.Tests
+{
+    public class YearsCountTests
+    {
+        [Fact]
+        public void CanCountYears()
+        {
+            int yearsDiff = 18;
+            DateTime adultDate = DateTime.Now.AddYears(-yearsDiff);
+            DateTime childDate = DateTime.Now.AddYears(-yearsDiff).AddDays(-2);
+
+            Assert.True(yearsDiff == adultDate.CountYears());
+            Assert.False(yearsDiff == childDate.CountYears());
+        }
+
+    }
+}
