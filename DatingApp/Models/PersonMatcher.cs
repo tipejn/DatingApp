6dace7edc299b9f26f72367fae588bdca1899daf
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 11:41:26 +0100

    added personMatcher

---
+++ DatingApp/Models/PersonMatcher.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.Models.Validators;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingApp.Models
+{
+    public class PersonMatcher
+    {
+        private List<IPersonValidator> validators = new List<IPersonValidator> { new AgeValidator(), new EyesColorValidator(), new HeightValidator() };
+        public Person Woman { get; set; }
+        public Person Man { get; set; }
+
+        public bool Matched()
+        {
+            return validators.All(v => v.Matched(Woman, Man));
+        }
+
+    }
+}
