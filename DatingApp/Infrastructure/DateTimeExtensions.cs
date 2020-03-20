4c7809ce385348e70168784d78bfc2dcbea5e42f
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 11:36:07 +0100

    added date time CountYears extension

---
+++ DatingApp/Infrastructure/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DatingApp.Infrastructure
+{
+    public static class DateTimeExtensions
+    {
+        public static int CountYears(this DateTime date)
+        {
+            var today = DateTime.Today;
+            int diff = today.Year - date.Year;
+            if (date.Date > today.AddYears(-diff))
+            {
+                diff--;
+            }
+            return diff;
+        }
+    }
+}
