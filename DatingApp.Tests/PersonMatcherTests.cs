ed6b9cb7f107ad922671e51fd300b82f0996773c
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 14:15:33 +0100

    added person matcher test

---
+++ DatingApp.Tests/PersonMatcherTests.cs
@@ -0,0 +1,51 @@
+﻿using DatingApp.Models;
+using DatingApp.Models.Validators;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DatingApp.Tests
+{
+    public class PersonMatcherTests
+    {
+        [Fact]
+        public void CanMatch()
+        {
+            Person w1 = new Person
+            {
+                BirthDate = new DateTime(1990, 1, 1),
+                EyesColor = EyesColor.Blue,
+                Height = 160
+            };
+
+            Person m1 = new Person
+            {
+                BirthDate = new DateTime(1993, 1, 1),
+                EyesColor = EyesColor.Blue,
+                Height = 169
+            };
+
+            Person w2 = new Person
+            {
+                BirthDate = new DateTime(1990, 1, 1),
+                EyesColor = EyesColor.Blue,
+                Height = 160
+            };
+
+            Person m2 = new Person
+            {
+                BirthDate = new DateTime(1996, 1, 1),
+                EyesColor = EyesColor.Green,
+                Height = 171
+            };
+
+            var validators = new List<IPersonValidator> { new AgeValidator(), new HeightValidator(), new EyesColorValidator() };
+
+            PersonMatcher matcher1 = new PersonMatcher(validators) { Woman = w1, Man = m1 };
+            PersonMatcher matcher2 = new PersonMatcher(validators) { Woman = w2, Man = m2 };
+
+            Assert.True(matcher1.Matched());
+            Assert.False(matcher2.Matched());
+        }
+    }
+}
