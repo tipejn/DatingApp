6f8985d6b0053f265ef65809f5719cf80b930dd7
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 11:40:09 +0100

    added valiators tests

---
+++ DatingApp.Tests/PersonValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using DatingApp.Models;
+using DatingApp.Models.Validators;
+using System;
+using Xunit;
+
+namespace DatingApp.Tests
+{
+    public class PersonValidatorTests
+    {
+
+        [Fact]
+        public void CanValidateAgeDiff()
+        {
+            Person w1 = new Person { BirthDate = new DateTime(1990, 1, 1) };
+            Person m1 = new Person { BirthDate = new DateTime(1992, 1, 1) };
+            Person w2 = new Person { BirthDate = new DateTime(1990, 1, 1) };
+            Person m2 = new Person { BirthDate = new DateTime(1996, 1, 1) };
+
+            AgeValidator validator = new AgeValidator();
+
+            Assert.True(validator.Matched(w1, m1));
+            Assert.False(validator.Matched(w2, m2));
+        }
+
+        [Fact]
+        public void CanValidateHeight()
+        {
+            Person w1 = new Person { Height = 1.6 };
+            Person m1 = new Person { Height = 1.69 };
+            Person w2 = new Person { Height = 1.60 };
+            Person m2 = new Person { Height = 1.71 };
+
+            HeightValidator validator = new HeightValidator();
+
+            Assert.False(validator.Matched(w1, m1));
+            Assert.True(validator.Matched(w2, m2));
+        }
+
+        [Fact]
+        public void CanValidateEyesColor()
+        {
+            Person w1 = new Person { EyesColor = EyesColor.Blue };
+            Person m1 = new Person { EyesColor = EyesColor.Blue };
+            Person w2 = new Person { EyesColor = EyesColor.Blue };
+            Person m2 = new Person { EyesColor = EyesColor.Green };
+
+            EyesColorValidator validator = new EyesColorValidator();
+
+            Assert.True(validator.Matched(w1, m1));
+            Assert.False(validator.Matched(w2, m2));
+        }
+    }
+}
