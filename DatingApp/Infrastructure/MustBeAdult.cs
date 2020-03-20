0b9978a2ba7ff2c3e2d8040330aac4803c5c78de
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 12:34:03 +0100

    added custom data annotation - MustBeAdult

---
+++ DatingApp/Infrastructure/MustBeAdult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Infrastructure
+{
+    public class MustBeAdult : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return value != null && DateTime.TryParse(value.ToString(), out DateTime birthDate) && IsAdult(birthDate);
+        }
+
+        private bool IsAdult(DateTime birthDate)
+        {
+            return birthDate.CountYears() >= 18;
+        }
+    }
+}
