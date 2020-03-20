9d317cd4e03aacf34a0fb193585058dab73ddad8
Michal Olszewski <michal.olszewski.90@gmail.com>
2020-03-20 12:39:11 +0100

    added result controller

---
+++ DatingApp/Controllers/ResultController.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.Models;
+using DatingApp.Models.Validators;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DatingApp.Controllers
+{
+    public class ResultController : Controller
+    {
+        private DatingRepository repo;
+        private IEnumerable<IPersonValidator> validators;
+        public ResultController(DatingRepository repo, IEnumerable<IPersonValidator> validators)
+        {
+            this.repo = repo;
+            this.validators = validators;
+        }
+        public ViewResult Index()
+        {
+            return View(new PersonMatcher(validators)
+            {
+                Woman = repo.GetPerson(Gender.Female),
+                Man = repo.GetPerson(Gender.Male)
+            });
+        }
+    }
+}
